db8b2e4147b557a0cb4ed859ff4fb36fe497dd71
tjwogud <rlatjsal78@gmail.com>
2022-10-02 01:36:17 +0900

    add RainbowImage component

---
+++ Assets/Components/RainbowImage.cs
@@ -0,0 +1,38 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HoloRush
+{
+    public class RainbowImage : MonoBehaviour
+    {
+        [SerializeField]
+        private Image m_image;
+        [SerializeField]
+        private SpriteRenderer m_spriteRenderer;
+        [SerializeField]
+        private float m_duration = 1;
+        [SerializeField]
+        private float m_saturation = 1;
+        [SerializeField]
+        private float m_value = 1;
+
+        private Tween tween;
+        private float m_hue;
+
+        private void Start()
+        {
+            tween = DOTween.To(() => m_hue, f => { m_hue = f; m_image.color = Color.HSVToRGB(f, m_saturation, m_value); }, 1, m_duration).OnComplete(() => m_hue = 0).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        }
+
+        private void OnEnable()
+        {
+            tween.Play();
+        }
+
+        private void OnDisable()
+        {
+            tween.Pause();
+        }
+    }
+}

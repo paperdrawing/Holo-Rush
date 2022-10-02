e5cf459ccd981d333fc5c74cc774f13175cdd51c
tjwogud <rlatjsal78@gmail.com>
2022-10-03 00:31:20 +0900

    fix RainbowImage component

--- Assets/Components/RainbowImage.cs
+++ Assets/Components/RainbowImage.cs
@@ -22,7 +22,20 @@ namespace HoloRush
 
         private void Start()
         {
-            tween = DOTween.To(() => m_hue, f => { m_hue = f; m_image.color = Color.HSVToRGB(f, m_saturation, m_value); }, 1, m_duration).OnComplete(() => m_hue = 0).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+            tween = DOTween.To(() => m_hue, f =>
+            {
+                Color color = Color.HSVToRGB(f, m_saturation, m_value);
+                if (m_image)
+                {
+                    color.a = m_image.color.a;
+                    m_hue = f; m_image.color = color;
+                }
+                if (m_spriteRenderer)
+                {
+                    color.a = m_spriteRenderer.color.a;
+                    m_hue = f; m_spriteRenderer.color = color;
+                }
+            }, 1, m_duration).OnComplete(() => m_hue = 0).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
         }
 
         private void OnEnable()
